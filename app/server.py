8a391aaa94debd33a32e389de2167d43d480a5cd
Andrew Ferrin <ferrinaj@vt.edu>
2024-04-17 21:32:51 -0400

    CORS for localhost:4200 and marineshrink.info

--- app/server.py
+++ app/server.py
@@ -3,6 +3,7 @@ from datetime import datetime
 
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import FileResponse
 
 from app.llm.controller import router as llm_router
@@ -11,6 +12,19 @@ from app.query.controller import data_router, query_router
 logging.basicConfig(level=logging.INFO)
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:4200",
+    "https://marineshrink.info",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/favicon.ico", include_in_schema=False)
